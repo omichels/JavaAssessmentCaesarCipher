fd5087cda21630abeb104e91513635313d44321f
Oliver Michels <oliver.michels@bechtle.com>
2023-10-18 11:15:47 +0200

    start here

--- python/caesar.py
+++ python/caesar.py
@@ -1,12 +1,8 @@
 def caesar(plainText, shift): 
     cipherText = ""
     for ch in plainText:
-        if ch.isalpha():
-            stayInAlphabet = ord(ch) + shift 
-            if stayInAlphabet > ord('z'):
-                stayInAlphabet -= 26
-            finalLetter = chr(stayInAlphabet)
-        cipherText += finalLetter
+
+     // start HERE
 
     return cipherText
 
